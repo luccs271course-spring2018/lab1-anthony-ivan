0c5f823802ae84a9bb6c860d1839468ada28e53c
Ivan Kavanagh <ikavanagh@luc.edu>
2017-09-09 23:24:57 +0000

    Added Personal Branch

--- src/main/java/hw/Main.java
+++ src/main/java/hw/Main.java
@@ -1,14 +1,36 @@
 package hw;
-
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class Main {
+  int n;
 
   public static void main(final String[] args) {
-    System.out.println("args = " + Arrays.asList(args));
-    final HelloWorld instance = new HelloWorld();
-    System.out.println(instance.getMessage());
-    System.out.println(instance.getYear());
-    System.out.println("bye for now");
+    
+          Scanner keyboard = new Scanner(System.in);
+		System.out.println("enter n integer and i'll calculate which ones a fizz and which ones a buzz!");
+		int n = keyboard.nextInt();
+		for(int i = 0; i < n; i++)
+		{
+			if(i % 5 == 0 && i % 3 == 0)
+			{
+				System.out.println("FizzBuzz");
+			}
+			else if(i % 3 == 0)
+			{
+				System.out.println("Fizz");
+				
+			}
+			else if(i % 5 == 0)
+			{
+				System.out.println("Buzz");
+			}
+			else
+			{
+				System.out.println(i);
+			}
+		}
   }
 }
+
+	
