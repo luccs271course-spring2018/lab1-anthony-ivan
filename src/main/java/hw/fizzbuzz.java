4d40a05f1a050717f046501aca2d7208a7e7483c
Ivan Kavanagh <ikavanagh@luc.edu>
2017-09-10 01:24:00 +0000

    lab1-part2

---
+++ src/main/java/hw/fizzbuzz.java
@@ -0,0 +1,36 @@
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class fizzbuzz{
+  int n;
+
+  public static void main(final String[] args) {
+    
+          Scanner keyboard = new Scanner(System.in);
+		System.out.println("enter n integer and i'll calculate which ones a fizz and which ones a buzz!");
+		int n = keyboard.nextInt();
+		for(int i = 0; i < n; i++)
+		{
+			if(i % 5 == 0 && i % 3 == 0)
+			{
+				System.out.println("FizzBuzz");
+			}
+			else if(i % 3 == 0)
+			{
+				System.out.println("Fizz");
+				
+			}
+			else if(i % 5 == 0)
+			{
+				System.out.println("Buzz");
+			}
+			else
+			{
+				System.out.println(i);
+			}
+		}
+  }
+}
+
+	
