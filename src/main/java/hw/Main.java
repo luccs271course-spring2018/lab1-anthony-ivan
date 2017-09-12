455fec55cc0ef24cf09d0175e0f93d67761ca6b3
Anthony LiFonti <alifonti@luc.edu>
2017-09-12 01:08:01 +0000

    part3 fizzbuzz

--- src/main/java/hw/Main.java
+++ src/main/java/hw/Main.java
@@ -4,11 +4,23 @@ import java.util.Arrays;
 
 public class Main {
 
-  public static void main(final String[] args) {
-    System.out.println("args = " + Arrays.asList(args));
-    final HelloWorld instance = new HelloWorld();
-    System.out.println(instance.getMessage());
-    System.out.println(instance.getYear());
-    System.out.println("bye for now");
+  public static void main(String[] args)
+  {
+    for(int i = 1; i <= 100; i++)
+    {
+      String output = "";
+      if(i % 3 == 0) {
+        output += "Fizz";
+      }
+      if(i % 5 == 0) {
+        output += "Buzz";
+      }
+      if(output == "") {
+        System.out.println(i);
+      }
+      else {
+        System.out.println(output);
+      }
+    }
   }
 }
